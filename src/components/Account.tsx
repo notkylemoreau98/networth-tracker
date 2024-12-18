0f0d89f1a9cfe016e3f9a85fb3f8602fe4fd1cc9
Kyle Moreau <kyle.moreau@foxdealer.com>
2024-12-18 11:18:46 -0800

    Edit and Delete functions

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,20 +1,51 @@
+import { FaTrashAlt } from "react-icons/fa";
+
 type AccountTypes = {
   id: number;
   name: string;
   balance: number;
+  isEditing: boolean;
+  onDeleteAccount: (id: number) => void;
+  onNameChange: (id: number, newName: string) => void;
+  onBalanceChange: (id: number, newBalance: number) => void;
 }
 
-const Account = ({ id, name, balance } : AccountTypes) => {
+
+const Account = ({ id, name, balance, isEditing, onDeleteAccount, onNameChange, onBalanceChange }: AccountTypes) => {
   const normalizeBalance = (balance: number) => {
     return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(balance);
   }
   
   return (
     <div key={id} className='flex justify-between'>
-      <span>{name}</span>
-      <span>{normalizeBalance(balance)}</span>
+      {isEditing ? (
+        <>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => onNameChange(id, e.target.value)}
+            className="border p-1 rounded bg-white"
+          />
+          <input
+            type="number"
+            value={balance}
+            onChange={(e) => onBalanceChange(id, parseFloat(e.target.value))}
+            className="border p-1 rounded bg-white"
+          />
+          <button
+            onClick={() => onDeleteAccount(id)}
+          >
+            <FaTrashAlt />
+          </button>
+        </>
+      ) : (
+        <>
+          <span>{name}</span>
+          <span>{normalizeBalance(balance)}</span>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
 export default Account;
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -20,44 +20,29 @@ interface GoalData {
 
 
 const DashBoard = () => {
+  const accountsLocalStorageKey: string = "accounts";
+  const goalsLocalStorageKey: string = "goals";
+  
   const [netWorth, setNetWorth] = useState(0);
   const [assets, setAssets] = useState(0);
   const [debts, setDebts] = useState(0);
-  const [accounts, setAccounts] = useState<AccountData[]>([]);
-  const [goals, setGoals] = useState<GoalData[]>([]);
   const [isAccountModalOpen, setIsAccountModalOpen] = useState<boolean>(false)
-  const [isGoalModalOpen, setIsGoalModalOpen] = useState<boolean>(false)
-  const accountsLocalStorageKey: string = "accounts";
+  const [isGoalModalOpen, setIsGoalModalOpen] = useState<boolean>(false);
+  const [isEditingAccount, setIsEditingAccount] = useState(false);
+  const [isEditingGoal, setIsEditingGoal] = useState(false);
+
+  const [accounts, setAccounts] = useState<AccountData[]>(() => {
+    const storedAccounts = localStorage.getItem(accountsLocalStorageKey);
+    return storedAccounts ? JSON.parse(storedAccounts) : [];
+  });
+  
+  const [goals, setGoals] = useState<GoalData[]>(() => {
+    const storedGoals = localStorage.getItem(goalsLocalStorageKey);
+    return storedGoals ? JSON.parse(storedGoals) : [];
+  });
 
   // Keep everything local w local storage first and then upgrade to DB and accounts later
-  
-  const tempAccountData: AccountData[] = [
-    {
-      id: 1,
-      name: 'Brokerage',
-      imageUrl: '',
-      balance: 4584.88
-    },
-    {
-      id: 2,
-      name: 'Roth IRA',
-      imageUrl: '',
-      balance: 15793.81
-    },
-    {
-      id: 3,
-      name: '401k',
-      imageUrl: '',
-      balance: 30670.62
-    },
-    {
-      id: 4,
-      name: 'HYSA',
-      imageUrl: '',
-      balance: 14179.82
-    }
-  ]
-
+  // Eventually break accounts and goals into separate sub pages
   const formatUSD = (value: number): string =>
     new Intl.NumberFormat('en-US', {
       style: 'currency',
@@ -78,7 +63,7 @@ const DashBoard = () => {
 
   const addGoal = (name: string, startingValue: number, goalValue: number) => {
     const newGoal: GoalData = {
-      id: accounts.length + 1,
+      id: goals.length + 1,
       name,
       startingValue,
       goalValue,
@@ -87,29 +72,94 @@ const DashBoard = () => {
     setIsGoalModalOpen(false);
   };
 
+  const handleSave = (type: string) => {
+    if (type === 'account') {
+      setIsEditingAccount(false);
+      localStorage.setItem(accountsLocalStorageKey, JSON.stringify(accounts));
+    } else {
+      setIsEditingGoal(false);
+      localStorage.setItem(goalsLocalStorageKey, JSON.stringify(goals));
+    }
+  };
+
+  const handleEdit = (type: string) => {
+    type === 'goal' ? setIsEditingGoal(!isEditingGoal) : setIsEditingAccount(!isEditingAccount);
+  };
+  
+
+  const handleAccountNameChange = (id: number, newName: string) => {
+    setAccounts(prev =>
+      prev.map(account =>
+        account.id === id ? { ...account, name: newName } : account
+      )
+    );
+  };
+
+  const handleAccountBalanceChange = (id: number, newBalance: number) => {
+    setAccounts(prev =>
+      prev.map(account =>
+        account.id === id ? { ...account, balance: newBalance } : account
+      )
+    );
+  };
+
+  const handleGoalNameChange = (id: number, newName: string) => {
+    setGoals(prev =>
+      prev.map(goal =>
+        goal.id === id ? { ...goal, name: newName } : goal
+      )
+    );
+  };
+
+  const handleGoalStartingBalanceChange = (id: number, newBalance: number) => {
+    setGoals(prev =>
+      prev.map(goal =>
+        goal.id === id ? { ...goal, startingValue: newBalance } : goal
+      )
+    );
+  };
+  
+  const handleGoalEndBalanceChange = (id: number, newBalance: number) => {
+    setGoals(prev =>
+      prev.map(goal =>
+        goal.id === id ? { ...goal, goalValue: newBalance } : goal
+      )
+    );
+  };
+
+  const handleDeleteAccount = (id: number) => {
+    setAccounts((prev) => prev.filter((account) => account.id !== id));
+  };
+  
+  const handleDeleteGoal = (id: number) => {
+    setGoals((prev) => prev.filter((goal) => goal.id !== id));
+  };
+
   useEffect(() => {
-    // Load accounts from local storage or use default data
     const storedAccounts = localStorage.getItem(accountsLocalStorageKey);
     if (storedAccounts) {
       setAccounts(JSON.parse(storedAccounts) as AccountData[]);
-    } else {
-      localStorage.setItem(accountsLocalStorageKey, JSON.stringify(tempAccountData));
-      setAccounts(tempAccountData);
+    }
+
+    const storedGoals = localStorage.getItem(goalsLocalStorageKey);
+    if (storedGoals) {
+      setGoals(JSON.parse(storedGoals) as GoalData[]);
     }
   }, []);
 
   useEffect(() => {
-    // Calculate total assets and net worth whenever accounts or debts change
+    localStorage.setItem(accountsLocalStorageKey, JSON.stringify(accounts));
+  }, [accounts]);
+
+  useEffect(() => {
+    localStorage.setItem(goalsLocalStorageKey, JSON.stringify(goals));
+  }, [goals]);
+
+  useEffect(() => {
     const totalAssets = accounts.reduce((sum, account) => sum + account.balance, 0);
     setAssets(totalAssets);
     setNetWorth(totalAssets - debts);
   }, [accounts, debts]);
-
-  useEffect(() => {
-    // Save accounts to local storage whenever they change
-    localStorage.setItem(accountsLocalStorageKey, JSON.stringify(accounts));
-  }, [accounts]);
-  
   
   return (
     <>
@@ -117,17 +167,17 @@ const DashBoard = () => {
        {/* Display Data */}
         {/* Total / Added this Year */}
         <section className='flex space-x-10'>
-          <div className='flex-1 bg-black text-white p-8 rounded-full text-2xl font-bold'>
+          <div className='flex-1 bg-black text-white p-8 rounded-xl text-2xl font-bold shadow-md'>
             <span>Net Worth:</span>
             {' '}
             <span>{formatUSD(netWorth)}</span>
           </div>
-          <div className='flex-1 bg-white p-8 rounded-full text-2xl font-bold'>
+          <div className='flex-1 bg-white p-8 rounded-xl text-2xl font-bold shadow-md'>
             {/* Ability to filter between week / month / year */}
             <span>Assets: </span>
             <span>{formatUSD(assets)}</span>
           </div>
-          <div className='flex-1 bg-white p-8 rounded-full text-2xl font-bold'>
+          <div className='flex-1 bg-white p-8 rounded-xl text-2xl font-bold shadow-md'>
             <span>Debts: </span>
             <span>{formatUSD(debts)}</span>
           </div>
@@ -139,10 +189,20 @@ const DashBoard = () => {
         </section> */}
 
         {/* These two sections side by side in a 8-4 col format */}
-        <section className='lg:flex space-x-5'>
+        <section className='lg:flex lg:space-x-5'>
           {/* Accounts */}
-          <div className='bg-white border border-1 border-black rounded-sm p-5 mt-10 lg:flex-[2]'>
-            <p className='text-left text-xl font-bold'>Accounts</p>
+          <div className='bg-white shadow-md rounded-md p-5 mt-10 lg:flex-[2]'>
+            <div className="flex justify-between">
+              <p className='text-left text-xl font-bold'>Accounts</p>
+
+              {accounts.length > 0 && (
+                <button
+                  onClick={() => handleEdit('account')}
+                >
+                  {isEditingAccount ? "Cancel" : "Edit"}
+                </button>
+              )}
+            </div>
 
             <div id='account-container' className='py-5 space-y-2'>
               {accounts.map(account => (
@@ -151,21 +211,44 @@ const DashBoard = () => {
                   id={account.id}
                   name={account.name}
                   balance={account.balance}
+                  isEditing={isEditingAccount}
+                  onDeleteAccount={handleDeleteAccount}
+                  onNameChange={handleAccountNameChange}
+                  onBalanceChange={handleAccountBalanceChange}
                 />
               ))}
             </div>
 
-            <button
-              onClick={() => setIsAccountModalOpen(true)}
-              className="bg-blue-500 text-white px-4 py-2 rounded mt-5"
-            >
-              Add New Account
-            </button>
+            {isEditingAccount ? (
+              <button
+                onClick={() => handleSave('account')}
+                className="bg-blue-500 text-white px-4 py-2 rounded mt-5"
+              >
+                Save
+              </button>
+            ) : (
+              <button
+                onClick={() => setIsAccountModalOpen(true)}
+                className="bg-blue-500 text-white px-4 py-2 rounded mt-5"
+              >
+                Add New Account
+              </button>
+            )}
           </div>
 
           {/* Goals */}
-          <div className='bg-white border border-1 border-black rounded-sm p-5 mt-10 lg:flex-1'>
-            <p className='text-left text-xl font-bold'>Goals</p>
+          <div className='bg-white shadow-md rounded-md p-5 mt-10 lg:flex-1'>
+            <div className="flex justify-between">
+              <p className='text-left text-xl font-bold'>Goals</p>
+
+              {goals.length > 0 && (
+                <button
+                  onClick={() => handleEdit('goal')}
+                >
+                  {isEditingGoal ? "Cancel" : "Edit"}
+              </button>
+              )}
+            </div>
 
             <div id='goal-container' className='py-5 space-y-2'>
               {goals.map(goal => (
@@ -175,16 +258,30 @@ const DashBoard = () => {
                   name={goal.name}
                   startingValue={goal.startingValue}
                   goalValue={goal.goalValue}
+                  isEditing={isEditingGoal}
+                  onDeleteGoal={handleDeleteGoal}
+                  onNameChange={handleGoalNameChange}
+                  onStartingBalanceChange={handleGoalStartingBalanceChange}
+                  onGoalBalanceChange={handleGoalEndBalanceChange}
                 />
               ))}
             </div>
 
-            <button
-              onClick={() => setIsGoalModalOpen(true)}
-              className="bg-blue-500 text-white px-4 py-2 rounded mt-5"
-            >
-              Add Goal
-            </button>
+            {isEditingGoal ? (
+              <button
+                onClick={() => handleSave('goal')}
+                className="bg-blue-500 text-white px-4 py-2 rounded mt-5"
+              >
+                Save
+              </button>
+            ) : (
+              <button
+                onClick={() => setIsGoalModalOpen(true)}
+                className="bg-blue-500 text-white px-4 py-2 rounded mt-5"
+              >
+                Add Goal
+              </button>
+            )}
           </div>
         </section>
 
@@ -204,4 +301,4 @@ const DashBoard = () => {
   )
 }
 
-export default DashBoard
+export default DashBoard;
