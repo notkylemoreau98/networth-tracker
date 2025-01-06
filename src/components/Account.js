e352ee329eeff77542e6f13ed403bb2672ee6432
Kyle Moreau <kyle.moreau@foxdealer.com>
2025-01-06 14:52:41 -0800

    Remove generated App.js and update .gitignore

--- src/components/Account.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx, Fragment as _Fragment, jsxs as _jsxs } from "react/jsx-runtime";
-import { FaTrashAlt } from "react-icons/fa";
-const Account = ({ id, name, balance, isEditing, onDeleteAccount, onNameChange, onBalanceChange }) => {
-    const normalizeBalance = (balance) => {
-        return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(balance);
-    };
-    return (_jsx("div", { className: 'flex justify-between', children: isEditing ? (_jsxs(_Fragment, { children: [_jsx("input", { type: "text", value: name, onChange: (e) => onNameChange(id, e.target.value), className: "border p-1 rounded bg-white" }), _jsx("input", { type: "number", value: balance, onChange: (e) => onBalanceChange(id, parseFloat(e.target.value)), className: "border p-1 rounded bg-white" }), _jsx("button", { onClick: () => onDeleteAccount(id), children: _jsx(FaTrashAlt, {}) })] })) : (_jsxs(_Fragment, { children: [_jsx("span", { children: name }), _jsx("span", { children: normalizeBalance(balance) })] })) }, id));
-};
-export default Account;
--- src/pages/login/index.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const LogIn = () => {
-    return (_jsx("div", { children: _jsx("p", { children: "Google Log in" }) }));
-};
-export default LogIn;
