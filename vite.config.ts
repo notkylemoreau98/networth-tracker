b70152ddf72ba42ccd03b36bd0777544796237cb
Kyle Moreau <kyle.moreau@foxdealer.com>
2025-01-06 14:44:47 -0800

    Remove App.js

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    extensions: ['.tsx', '.js', '.jsx', '.ts'],
   }
 })
