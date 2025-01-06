7e6985bfc530cdabf675c8c852599d6732d21218
Kyle Moreau <kyle.moreau@foxdealer.com>
2025-01-06 14:38:15 -0800

    AWS fix 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   resolve: {
     extensions: ['.tsx', '.js', '.jsx', '.ts'],
-  },
-  server: {
-    watch: {
-      usePolling: true,
-      interval: 100,
-    },
   }
 })
